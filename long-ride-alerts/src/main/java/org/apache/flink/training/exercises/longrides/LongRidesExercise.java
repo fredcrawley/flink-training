d6cd851f2dabca18857df0e09779304ea8a93c6a
10172 <wen.xin@datasw.com>
2023-10-13 11:17:19 +0800

    4个练习 过了

--- long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
+++ long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
@@ -21,6 +21,10 @@ package org.apache.flink.training.exercises.longrides;
 import org.apache.flink.annotation.VisibleForTesting;
 import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.StateTtlConfig;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -30,7 +34,6 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
-import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 import org.apache.flink.util.Collector;
 
 import java.time.Duration;
@@ -47,7 +50,9 @@ public class LongRidesExercise {
     private final SourceFunction<TaxiRide> source;
     private final SinkFunction<Long> sink;
 
-    /** Creates a job using the source and sink provided. */
+    /**
+     * Creates a job using the source and sink provided.
+     */
     public LongRidesExercise(SourceFunction<TaxiRide> source, SinkFunction<Long> sink) {
         this.source = source;
         this.sink = sink;
@@ -98,17 +103,51 @@ public class LongRidesExercise {
     @VisibleForTesting
     public static class AlertFunction extends KeyedProcessFunction<Long, TaxiRide, Long> {
 
+        public ValueState<Long> tsState;
+
+        public final int maxTs = 60 * 60 * 1000 * 2;
+
+        public final int wmTs = 60 * 1000;
+
+        public final int exceedDay = 15;
+
         @Override
         public void open(Configuration config) throws Exception {
-            throw new MissingSolutionException();
+            StateTtlConfig ttlConfig = StateTtlConfig
+                    .newBuilder(Time.days(exceedDay))
+                    .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+                    .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
+                    .build();
+            ValueStateDescriptor<Long> tsStateDs = new ValueStateDescriptor<>("tsStateDs", Long.class);
+            tsStateDs.enableTimeToLive(ttlConfig);
+            tsState = getRuntimeContext().getState(tsStateDs);
+
         }
 
         @Override
         public void processElement(TaxiRide ride, Context context, Collector<Long> out)
-                throws Exception {}
+                throws Exception {
+            Long tsStateValue = tsState.value();
+            long ts_now = ride.getEventTimeMillis();
+            if (null != tsStateValue) {
+                Long starEndTime = Math.abs(tsStateValue - ts_now);
+                if (starEndTime > maxTs) {
+                    out.collect(context.getCurrentKey());
+                    tsState.clear();
+                } else {
+                    context.timerService().deleteEventTimeTimer(tsStateValue + maxTs + wmTs);
+                }
+            } else {
+                tsState.update(ts_now);
+                context.timerService().registerEventTimeTimer(ts_now + maxTs + wmTs);
+            }
+        }
 
         @Override
         public void onTimer(long timestamp, OnTimerContext context, Collector<Long> out)
-                throws Exception {}
+                throws Exception {
+            out.collect(context.getCurrentKey());
+            tsState.clear();
+        }
     }
 }
